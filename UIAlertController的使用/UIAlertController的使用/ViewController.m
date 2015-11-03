608eb8276e824d7ea42232246d50c3894eda6c77
EdwinXiang <306229443@qq.com>
2015-11-03 14:54:22 +0800

    添加下拉列表的使用

--- UIAlertController的使用/UIAlertController的使用/ViewController.m
+++ UIAlertController的使用/UIAlertController的使用/ViewController.m
@@ -8,8 +8,13 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UINavigationControllerDelegate,UIImagePickerControllerDelegate>
+
+@property(nonatomic,strong)UIImagePickerController *imagepicker;
+
+@property(nonatomic,strong)UIImageView *imageView;
 - (IBAction)btnclick:(id)sender;
+- (IBAction)sheetclick:(id)sender;
 
 @end
 
@@ -18,6 +23,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    _imageView = [[UIImageView alloc]initWithFrame:CGRectMake(50, 100, self.view.frame.size.width-100, self.view.frame.size.height/2)];
+    [self.view addSubview:_imageView];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -61,6 +70,62 @@
     
     [alert addAction:action];
     [self presentViewController:alert animated:YES completion:nil];
+}
+
+- (IBAction)sheetclick:(id)sender {
+    
+    //初始化一个图片控制器
+    _imagepicker = [[UIImagePickerController alloc]init];
+    
+    //添加下拉列表
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:nil message:nil preferredStyle:UIAlertControllerStyleActionSheet];
+    //添加拍照按钮
+    UIAlertAction *cameraAction = [UIAlertAction actionWithTitle:@"拍照" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        
+        _imagepicker.sourceType = UIImagePickerControllerSourceTypeCamera;
+        _imagepicker.delegate = self;
+        _imagepicker.allowsEditing = YES;//设置图片可编辑
+        [self presentViewController:_imagepicker animated:YES completion:nil];
+        
+    }];
+    //添加进入系统相册按钮
+    UIAlertAction *albumAction = [UIAlertAction actionWithTitle:@"从手机相册选择" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        
+        _imagepicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+        _imagepicker.delegate = self;
+        _imagepicker.allowsEditing = YES;//设置图片可编辑
+        [self presentViewController:_imagepicker animated:YES completion:nil];
+        
+        
+    }];
+    //添加取消按钮
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+        
+    }];
+    
+    //将按钮加入下拉列表
+    [alertController addAction:cameraAction];
+    [alertController addAction:albumAction];
+    [alertController addAction:cancelAction];
+    
+    [self presentViewController:alertController animated:YES completion:nil];
+}
+
+//选取图片的方法
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    [[picker parentViewController]dismissViewControllerAnimated:YES completion:^{
+        //
+    }];
+//    [[UIApplication sharedApplication]setStatusBarHidden:NO];
+    //找到选中的修改后那张照片
+    UIImage *image = [info objectForKey:UIImagePickerControllerEditedImage];
+    _imageView.image = image;
+    
+    
+    [self dismissViewControllerAnimated:YES completion:^{
+        //
+    }];
 }
 
 -(void)alertTextFieldDidChange:(NSNotification *)fication{
